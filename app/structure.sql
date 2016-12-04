9cae5f4c42f352a77127c4fb504621ce3e484e7c
MSI-GT60-2OD\libor.drapal <libor.drapal@email.cz>
2016-12-04 21:30:14 +0100

    updated db structure

--- app/structure.sql
+++ app/structure.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Nov 23, 2016 at 06:55 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `pusici`
 --
@@ -31,7 +13,8 @@ USE `pusici`;
 CREATE TABLE `tokens` (
   `user_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `access_token` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL
+  `refresh_token` varchar(44) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `expire` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
