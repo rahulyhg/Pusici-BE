8daf50321bab2a10519a9ac9cffc5a383d054e5b
Libor Drapal <libor.drapal@email.cz>
2016-12-29 16:38:11 +0100

    implemented endpoints get user profile and password change

--- app/Api/users.php
+++ app/Api/users.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
  * Get all Users
  *
  * - simplified query message parameters can be used (?filter=first_name:=:John&orderby=last_name:asc)
+ * - required permission: 'users' or 'users-read'
  *
  * 200 - Return array of users
  * 400 - database_error
@@ -58,6 +59,8 @@ $app->get('/api/users', function ($request, $response) {
 /**
  * Get User with id
  *
+ * - required permission: 'users' or 'users-read'
+ *
  * 200 - Return User
  * 404 - user_not_found
  * 500 - Internal Server Error
@@ -80,7 +83,34 @@ $app->get('/api/users/{id}', function ($request, $response) {
 })->add(new \App\Middleware\Permissions($container, ['users', 'users-read']));
 
 /**
+ * Get logged-in User profile
+ *
+ * 200 - Return User profile
+ * 400 - jwt_not_found
+ * 500 - Internal Server Error
+ */
+$app->get('/api/users/myself/profile', function ($request, $response) {
+    if ($this->has('jwt')) {
+        try {
+            $user = User::where('email', '=', $this->jwt->user->email)->first();
+        } catch (\PDOException $e) {
+            return code_500($response, $e->getMessage());
+        }
+        $data = [];
+        if (isset($user)) {
+            $data[first_name] = $user->first_name;
+            $data[last_name] = $user->last_name;
+            $data[email] = $user->email;
+        }
+        return code_200($response, $data);
+    }
+    return code_400($response, 'jwt_not_found');
+});
+
+/**
  * Create new User
+ *
+ * - required permission: 'users'
  *
  * 201 - Return User id
  * 400 - wrong_input, email_exist
@@ -131,6 +161,8 @@ $app->post('/api/users', function ($request, $response) {
 /**
  * Update User with id
  *
+ * - required permission: 'users'
+ *
  * 204 - User updated
  * 400 - wrong_input, email_exist
  * 404 - user_not_found
@@ -170,7 +202,51 @@ $app->put('/api/users/{id}', function ($request, $response) {
 })->add(new \App\Middleware\Permissions($container, ['users']));
 
 /**
+ * Change password for logged-in User
+ *
+ * 204 - password successfully changed
+ * 400 - jwt_not_found, user_not_found, password_mismatched, wrong_input
+ * 500 - Internal Server Error
+ */
+$app->put('/api/users/myself/password-change', function ($request, $response) {
+    $oldPassword = $request->getParsedBodyParam('old_password');
+    $newPassword = $request->getParsedBodyParam('new_password');
+
+    if ($this->has('jwt')) {
+        try {
+            $user = User::where('email', '=', $this->jwt->user->email)->first();
+        } catch (\PDOException $e) {
+            return code_500($response, $e->getMessage());
+        }
+
+        if (isset($user)) {
+            $userData = $user->userData;
+            if (password_verify($oldPassword, $userData->password))
+            {
+                $userData->password = $newPassword;
+
+                if ($userData->validate()) {
+                    $userData->password = password_hash($userData->password, PASSWORD_BCRYPT);
+                    try {
+                        $userData->save();
+                    } catch (\PDOException $e) {
+                        return code_500($response, $e->getMessage());
+                    }
+                    return code_204($response);
+                }
+                return code_400($response, 'wrong_input', $userData->errors());
+            }
+            return code_400($response, 'password_mismatched', 'The old password is incorrect.');
+        }
+        return code_400($response, 'user_not_found');
+    }
+    return code_400($response, 'jwt_not_found');
+});
+
+/**
  * Delete User with id
+ *
+ * - required permission: 'users'
  *
  * 204 - User deleted
  * 404 - user_not_found
